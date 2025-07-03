81efa3b9e0017e4d746f328452d4733ae1d932da
nguynguyn <nnbn0903539221@gmail.com>
2025-07-03 18:04:32 +0700

    Create is-palindrome.js

---
+++ is-palindrome.js
@@ -0,0 +1,5 @@
+// Kiểm tra chuỗi có phải palindrome không
+function isPalindrome(str) {
+  const clean = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return clean === clean.split('').reverse().join('');
+}
