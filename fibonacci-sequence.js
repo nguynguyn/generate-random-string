f6df2b152be787713330cb030934110960aace6a
nguynguyn <nnbn0903539221@gmail.com>
2025-07-03 18:04:08 +0700

    Create fibonacci-sequence.js

---
+++ fibonacci-sequence.js
@@ -0,0 +1,8 @@
+// Tạo dãy số Fibonacci với n phần tử
+function fibonacci(n) {
+  const seq = [0, 1];
+  for (let i = 2; i < n; i++) {
+    seq.push(seq[i - 1] + seq[i - 2]);
+  }
+  return seq.slice(0, n);
+}
